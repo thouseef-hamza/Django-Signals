447ed4ca302daba538f8c2685ef119384f20dbf5
Thouseef <thousithouseef731@gmail.com>
2023-07-03 12:18:52 +0530

    pre_init,post_init,pre_save,post_save,pre_delete,post_delete

--- blog/signals.py
+++ blog/signals.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-
+from django.db.models.signals import pre_init,post_init,pre_save,post_save,pre_delete,post_delete
 
 @receiver(user_logged_in,sender=User)
 def login_success(sender,request,user,**kwargs):
-    print('-------------------------------------')
+    print('--------------------------------------------------------------------------------------------------------------------------->')
     print("Logged-in Signal....... Welcome To Meta World")
     print("Sender:  ",sender)
     print("Request:  ",request)
@@ -17,7 +17,7 @@ def login_success(sender,request,user,**kwargs):
     
 @receiver(user_logged_out,sender=User)
 def log_out(sender,request,user,**kwargs):
-    print('-------------------------------------')
+    print('--------------------------------------------------------------------------------------------------------------------------->')
     print("Logged-Out Signal...Thank You For Your Valuable Time With Our World")
     print("Sender:  ",sender)
     print("Request:  ",request)
@@ -29,7 +29,7 @@ def log_out(sender,request,user,**kwargs):
 
 @receiver(user_login_failed)
 def login_failed(sender,credentials,request,**kwargs):
-    print('-------------------------------------')
+    print('-------------------------------------------------------------------------------------------------------------------------->')
     print("Log-In-Failed Signal...Oh Oh Invalid Credentials,Correct Your Credentials To Access Our Meta World")
     print("Sender:  ",sender)
     print("Request:  ",request)
@@ -37,3 +37,74 @@ def login_failed(sender,credentials,request,**kwargs):
     print("Kwargs:  ",kwargs)       
 
 # user_login_failed.connect(login_failed)
+
+@receiver(pre_save,sender=User)
+def at_beginning_save(sender,instance,**kwargs):
+    print("------------------------------------------------------------------------------------------------------------------------->")
+    print("Pre Save Signal.............")
+    print("Sender:  ",sender)
+    print("Instance:  ",instance)
+    print("Kwargs:  ",kwargs)
+
+# pre_save.connect(at_beginning_save,sender=User)
+
+@receiver(post_save,sender=User)
+def at_ending_save(sender,instance,created,**kwargs):
+    if created:
+        print("----------------------------------------------------------------------------------------------------------------------->")
+        print("Post Save Signal.....")
+        print("Sender:  ",sender)
+        print("Instance:  ",instance)
+        print("Created:  ",created)
+        print("Kwargs:  ",kwargs)
+    else:
+        print("----------------------------------------------------------------------------------------------------------------------->")
+        print("Post Save Signal.......")
+        print("UPDATED")
+        print("Sender:  ",sender)
+        print("Instance:  ",instance)
+        print("Created:  ",created)
+        print("Kwargs:  ",kwargs)
+
+# post_save.connect(at_ending_save,sender=User)
+
+@receiver(pre_delete,sender=User)
+def at_beginning_delete(sender,instance,**kwargs):
+    print("------------------------------------------------------------------------------------------------------------------------->")
+    print("Pre Delete Signal.............")
+    print("Sender:  ",sender)
+    print("Instance:  ",instance)
+    print("Kwargs:  ",kwargs)
+    
+# pre_delete.connect(at_beginning_delete,sender=User)
+
+@receiver(post_delete,sender=User)
+def at_ending_delete(sender,instance,**kwargs):
+    print("------------------------------------------------------------------------------------------------------------------------->")
+    print("Post Delete Signal.............")
+    print("Sender:  ",sender)
+    print("Instance:  ",instance)
+    print("Kwargs:  ",kwargs)
+    
+# post_delete.connect(at_ending_delete,sender=User)
+
+
+@receiver(pre_init,sender=User)
+def at_beginning_init(sender,*args,**kwargs):
+    print("------------------------------------------------------------------------------------------------------------------------->")
+    print("Pre Init Signal.............")
+    print("Sender:  ",sender)
+    print("Args:  ",args)
+    print("Kwargs:  ",kwargs)
+    
+# pre_init.connect(at_beginning_init,sender=User)
+
+@receiver(post_init,sender=User)
+def at_ending_init(sender,*args,**kwargs):
+    print("------------------------------------------------------------------------------------------------------------------------->")
+    print("Post Init Signal.............")
+    print("Sender:  ",sender)
+    print("Args:  ",args)
+    print("Kwargs:  ",kwargs)
+    
+# post_init.connect(at_ending_init,sender=User)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,122 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#========================================================================================================================================
+"""
+pre_save---------------------------------------------------------------------------------------------------------->
+django.db.models.signals.pre_save
+
+This is sent at the beginning of a model's save() method.
+
+Arguments sent with this signal:
+
+<>sender
+    The model class.
+<>instance
+    The actual instance being saved.
+<>raw
+    A boolean; True if the model is saved exactly as presented (i.e. when loading a fixture). 
+    One should not query/modify other records in the database as the database might not be in a consistent state yet.
+<>using
+    The database alias being used.
+<>update_fields
+    The set of fields to update as passed to Model.save(), or None if update_fields wasn’t passed to save().
+
+"""
+#===============================================================================================================================================================
+"""
+post_save----------------------------------------------------------------------------------------------------------------->
+django.db.models.signals.post_save
+
+Like pre_save, but sent at the end of the save() method.
+
+Arguments sent with this signal:
+
+<>sender
+    The model class.
+<>instance
+    The actual instance being saved.
+<>created
+    A boolean; True if a new record was created.
+<>raw
+    A boolean; True if the model is saved exactly as presented (i.e. when loading a fixture). 
+    One should not query/modify other records in the database as the database might not be in a consistent state yet.
+<>using
+    The database alias being used.
+<>update_fields
+    The set of fields to update as passed to Model.save(), or None if update_fields wasn’t passed to save().
+"""
+#======================================================================================================================================
+""" 
+pre_delete--------------------------------------------------------------------------------------------->
+django.db.models.signals.pre_delete
+
+Sent at the beginning of a model’s delete() method and a queryset’s delete() method.
+
+Arguments sent with this signal:
+
+<>sender
+    The model class.
+<>instance
+    The actual instance being deleted.
+<>using
+    The database alias being used.
+<>origin
+    The origin of the deletion being the instance of a Model or QuerySet class.
+"""
+#========================================================================================================================================
+"""
+post_delete---------------------------------------------------------------------------------------->
+django.db.models.signals.post_delete
+
+Like pre_delete, but sent at the end of a model’s delete() method and a queryset’s delete() method.
+
+Arguments sent with this signal:
+
+<>sender
+    The model class.
+<>instance
+    The actual instance being deleted.
+
+^^^^^^^                                                                                                            ^^^^^^^^^^^
+        Note that the object will no longer be in the database, so be very careful what you do with this instance.
+^^^^^^^                                                                                                            ^^^^^^^ 
+
+<>using
+The database alias being used.
+<>origin
+    The origin of the deletion being the instance of a Model or QuerySet class.
+"""
+#=======================================================================================================================================
+"""
+pre_init-------------------------------------------------------------------------------------------->
+django.db.models.signals.pre_init
+
+Whenever you instantiate a Django model, this signal is sent at the beginning of the model’s __init__() method.
+
+Arguments sent with this signal:
+
+<>sender
+    The model class that just had an instance created.
+<>args
+    A list of positional arguments passed to __init__().
+<>kwargs
+    A dictionary of keyword arguments passed to __init__().
+
+"""
+#========================================================================================================================================
+"""
+post_init--------------------------------------------------------------------------------------------->
+django.db.models.signals.post_init
+
+Like pre_init, but this one is sent when the __init__() method finishes.
+
+Arguments sent with this signal:
+
+<>sender
+    As above: the model class that just had an instance created.
+<>instance
+    The actual instance of the model that’s just been created.
+
+"""
